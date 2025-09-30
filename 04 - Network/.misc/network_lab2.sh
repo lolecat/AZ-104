c64b60bbe5be6024c4dc7a9b03ff44520737fdfa
unknown <lecat@louis@outlook.com>
2025-09-30 22:14:51 +0200

    typo script lab2

--- 04 - Network/.misc/network_lab2.sh
+++ 04 - Network/.misc/network_lab2.sh
@@ -8,9 +8,6 @@ do
     j=$((i + 4))
     ip="192.168.40.$j"
 
-    # Clean up known_hosts for this IP to avoid verification conflicts
-    ssh-keygen -R "$ip"
-
     ssh -o StrictHostKeyChecking=no -i /home/louis/.ssh/id_rsa louis@$ip bash -c "'
         set -e
         export DEBIAN_FRONTEND=noninteractive
